5c76ab9ef9fbf45f48e7442c6698f00f2924e049
Shahed Mehbub <shahed@theclinician.com>
2022-04-27 08:20:22 +0600

    init

---
+++ src/HelloWorldLambda/Function.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace HelloWorldLambda
+{
+    public class Function
+    {
+        
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public string FunctionHandler(ILambdaContext context)
+        {
+            return "Hello World from CodePipeline!!!";
+        }
+    }
+}
